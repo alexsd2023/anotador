f1c028f0802b8df1649054c417e8d850482836d5
alexsd2023 <156823731+alexsd2023@users.noreply.github.com>
2024-01-22 00:01:22 +0100

    Add files via upload

--- Anotador.py
+++ Anotador.py
@@ -2,16 +2,34 @@
 # coding: utf-8
 
 import streamlit as st
-st.set_page_config(layout="wide")
-
+import base64
 from streamlit_option_menu import option_menu
 from views import Entities, Annotate, ViewAnnotations, NER, FileLogs, Anotar, Statistics, Users
 
 import streamlit_authenticator as stauth
+
+st.set_page_config(layout="centered", page_title="Annotation Tool",  menu_items={'About':"Lancaster Annotation Tool. All copyrights reserved"})
+
 import yaml
 from yaml.loader import SafeLoader
 with open('./credentials.yaml') as file:
     config= yaml.load(file, Loader= SafeLoader)
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    [data-testid="stAppViewContainer"] {
+    background-image: url("data:image/png;base64,%s") ;
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
 
 authenticator = stauth.Authenticate(
     config['credentials'],
@@ -22,8 +40,11 @@ authenticator = stauth.Authenticate(
 )
 
 name, authentication_status, username= authenticator.login('Login', 'main')
+#set_background('./background.png')
 
 if authentication_status:
+
+    
     
     authenticator.logout('Logout', 'sidebar')
     with st.sidebar:
@@ -51,7 +72,12 @@ if authentication_status:
     
 
 elif authentication_status == False:
+    
     st.error('Username/password is incorrect')
 elif authentication_status == None:
     st.warning('Please enter your username and password')
+
+
+
+
 
