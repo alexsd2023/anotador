be98f8bc9914efe27948e379c6a0e923ec64a736
alexsd2023 <156823731+alexsd2023@users.noreply.github.com>
2024-01-22 10:08:06 +0100

    Add files via upload

--- Anotador.py
+++ Anotador.py
@@ -40,7 +40,7 @@ for un, name, pw in zip(usernames, names, hashed_passwords):
     user_dict = {"name":name,"password":pw}
     credentials["usernames"].update({un:user_dict})
 
-authenticator = stauth.Authenticate(credentials, "cokie_name", "random_key", cookie_expiry_days=30)
+authenticator = stauth.Authenticate(credentials, "cookie_name", "random_key", cookie_expiry_days=30)
 
 #authenticator = stauth.Authenticate(
 #    config['credentials'],
