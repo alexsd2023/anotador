97e523473079a2229d425ceaf5d89c784a2444a3
alexsd2023 <156823731+alexsd2023@users.noreply.github.com>
2024-03-27 09:05:15 +0100

    Update Anotador.py

--- Anotador.py
+++ Anotador.py
@@ -18,7 +18,7 @@ with st.sidebar:
 import yaml
 from yaml.loader import SafeLoader
 
-with open('config.yaml') as file:
+with open('credentials.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = stauth.Authenticate(
