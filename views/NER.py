ac4feba698d39f9d7770b0c0c45512121d0c6eb7
alexsd2023 <156823731+alexsd2023@users.noreply.github.com>
2024-09-20 11:27:17 +0200

    Add files via upload

--- views/NER.py
+++ views/NER.py
@@ -7,50 +7,66 @@ from spacy.tokens import Span
 import streamlit.components.v1 as components
 import tempfile
 from pathlib import Path
+import tensorflow as tf
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def read_htmlfile(html):
+    file = open(html, "r")
+    content = file.read()
+    soup=BeautifulSoup(content)
+    file.close()
+    return soup.get_text()
 
 def run():
-        
-        tab1, tab2= st.tabs(['Entities', 'Parts of speech'])
-        
-        if 'annot_file' in st.session_state:
-            uploaded_file= st.session_state['annot_file']
-            texto= ""
-            if uploaded_file is not None:
-                print(uploaded_file) 
-                with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-                    #st.markdown("# Original text file")
-                    fp = Path(tmp_file.name)
-                    fp.write_bytes(uploaded_file.getvalue())
-                    #print(fp)
+    if 'index' in st.session_state.keys():
+        index= st.session_state['index']
+    else:
+        index= 0  
+    modelname = st.radio(
+         "Select a model",
+        ["Spacy Ruler (Sentences)", "DECM annotations with Paragraphs"],
+         index=index,
+    )
+    if modelname == 'Spacy Ruler (Sentences)':
+        st.session_state['index']= 0
+        trained_nlp= spacy.load("./models/model_entities_ruler/model-last/")
+    elif modelname == 'DECM annotations with Paragraphs':
+        trained_nlp= spacy.load("./models/model_toponyms_patterns/model-last/")
+        st.session_state['index']= 1
 
+    uploaded_file= st.file_uploader("Choose a raw data file", type=['html', 'htm', 'txt', 'text'],  accept_multiple_files= False)
+    texto= ""
+    if 'uploaded_file' in st.session_state.keys():
+        uploaded_file= st.session_state['uploaded_file']
 
-                with open(fp,'r') as file:
-                    texto = " ".join(line for line in file)
-                    print(texto)
-                    
-        
-            #with tab2:
-            #st.header('Dependencies')
-            #svg= displacy.render(doc, style='dep', jupyter=False)
-            #st.image(svg, width= 400, use_column_width= 'never')
-            
-            #with tab3:
-                
-            #st.header('Dependency visualizer')
-            st.text_area("Annotated text", value= texto,  key= "text", height=520)
-                
-            
-            nlp = spacy.load('es_core_news_sm')
-            doc= nlp(texto)
-            
-            
-            ent_html= displacy.render(doc, style="ent", jupyter= False)
+    if uploaded_file is not None:
+        print(uploaded_file) 
+        st.session_state['uploaded_file']= uploaded_file
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            #st.markdown("# Original text file")
+            fp = Path(tmp_file.name)
+            fp.write_bytes(uploaded_file.getvalue())
+            #print(fp)
+        flag= True  
+        with open(fp,'r') as file:
+            if Path(uploaded_file.name).suffix in ['txt', 'text']:
+                texto = " ".join(line for line in file)
+            else:  
+                texto= read_htmlfile(fp)    
+    
 
-            if tab1:
-                st.markdown(ent_html, unsafe_allow_html= True)
-                displacy.serve(doc, style="dep")
-            if tab2:
-                st.markdown(ent_html, unsafe_allow_html= True)
-                displacy.serve(doc, style="dep")
-                 
-                
+    doc= trained_nlp(texto)
+
+    #st.text_area("Annotated text", value= texto,  key= "text", height=520)
+    data= []
+    for ent in doc.ents:
+        #print (ent.text, ent.label_, ent.start_char, ent.end_char)
+        data.append([ent.text, ent.label_, ent.start_char, ent.end_char])
+    df_entities= pd.DataFrame(data, columns= ['Text', 'Label', 'Start char', 'End char'])
+    st.session_state['df_entities']= df_entities
+
+    ent_html= displacy.render(doc, style="ent", jupyter= False)
+    st.markdown(ent_html, unsafe_allow_html= True)
+
--- views/Statistics.py
+++ views/Statistics.py
@@ -7,7 +7,7 @@ import streamlit.components.v1 as components
 from utils import annotate_txt
 import matplotlib.colors as mcolors
 import glob
-
+import os
 def run():
     
     if 'df_entities' in st.session_state.keys():
@@ -16,9 +16,12 @@ def run():
          
     else:
         df_entities= None
+        labels= []
     #print(labels)
+    temp= None
     options= ['All']
-    options+= labels.tolist()
+    if len(labels) > 0:
+        options+= labels.tolist()
     option= st.selectbox('Select an entity', options)   
     if option == 'All':
         st.table(df_entities)            
@@ -30,8 +33,32 @@ def run():
         temp.set_index('No.', inplace= True)
         st.table(temp)
 
-    options= glob.glob('../COLLECTIONS/*')
-    for collection in glob.glob('../COLLECTIONS/*.csv'):
-        print(collection)
-    print('collections:', options)
-    collections= st.selectbox('Select a collection', options) 
+    options= []
+    basepath= './COLLECTIONS/'
+    for collection in glob.glob('./COLLECTIONS/*.csv'):
+        options.append(os.path.basename(collection))
+    
+    collection= st.selectbox('Select a collection', options) 
+    df_collection= pd.read_csv(basepath+collection)
+    #st.dataframe(df_collection['Text'])
+
+    entities= []
+    for index in df_collection.index:
+        #st.write(df_collection.loc[index, 'Text'])
+        entities.append(df_collection.loc[index, 'Text'])
+    total_coincidencias= 0
+    nowords= []
+    if temp is not None:
+        for index in temp.index:
+            if temp.loc[index, 'Text'] in entities:
+                total_coincidencias+=1
+            else:
+                palabra= temp.loc[index, 'Text']  
+                nowords= nowords +[palabra]
+                     
+        st.write('Total de coincidencias: ', total_coincidencias)
+        st.write('No coincidencias: ', len(temp.index)-total_coincidencias)
+        s= ''
+        for i in nowords:
+            s += "-" + i + "\n"
+        st.markdown(s)
